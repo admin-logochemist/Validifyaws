d9b452dc5e0081a6b9676cd4b55dacee82831ed5
Taichee Rixvi <85700339+admin-logochemist@users.noreply.github.com>
2022-04-01 05:44:01 +0500

    Ghayas

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,18 @@
 var express = require('express');
 var router = express.Router();
 var ccxt = require ('ccxt')
-let huobipro  = new ccxt.huobipro ()
+const chalk = require('chalk');
+let huobipro  = new ccxt.binance ()
 /* GET users listing. */
 router.get('/',async function(req, res, next) {
  const currency=req.query.currency;
  
 //  console.log(huobipro);
 //  const exchangeId = 'binance', exchangeClass = ccxt[exchangeId]
-const exchangeId = 'binance'
-const exeth= await huobipro.fetchTrades (currency)
+const exchangeId = 'un'
+const exeth= await huobipro.fetchTrades (currency);
+const market =await huobipro.fetchMarkets()
+
 
 res.header("Access-Control-Allow-Origin", "*");
 res.send(exeth);
